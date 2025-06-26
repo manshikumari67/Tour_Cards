690fc954fce8bb0583912d962428f74876882cec
manshikumari67 <manshikumari345@gmail.com>
2025-06-26 23:22:34 +0530

    data added only

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const data=[
+    {
+        id : 1,
+        name : "Agra",
+        info : "Agra, located in Uttar Pradesh, India, is a major tourist destination renowned for its Mughal-era architecture, particularly the Taj Mahal, a UNESCO World Heritage Site. Alongside the Taj Mahal, Agra Fort and Fatehpur Sikri are also UNESCO World Heritage Sites, making Agra a popular destination for those interested in history and architecture. ",
+        image : "https://foodandtravel.com/imager/hours/219609/Ministry-of-Tourism-Govt-of-India-595161_Taj-Mahal-Uttar-Pradesh-Aerial_821631bb23140209ea99c8f810f75634.jpg",
+        Price : "35,780",
+       
+    },
+    {
+      id : 2,
+      name : "Jaipur",
+        info : "Jaipur, also known as the Pink City is a major tourist destination in India, renowned for its historical forts, palaces, vibrant bazaars, and rich cultural heritage. Key attractions include Amer Fort, Hawa Mahal, City Palace, Jantar Mantar, and Nahargarh Fort, all showcasing the city's regal past and architectural splendor. Jaipur is also a shopper's paradise, with bustling markets like Johari Bazaar and Bapu Bazaar offering traditional crafts, textiles, and jewelry.  ",
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuDD8CIjBDuVukV24jBDSDnW6-DUu3qrzpeQ&s",
+        Price : "82,670",  
+    },
+    {
+      id : 3,
+      name : "Goa",
+        info : "Goa, a coastal state in India, is a popular tourist destination known for its beaches, nightlife, historical sites, and unique blend of Portuguese and Indian cultures. It attracts both domestic and international travelers with its diverse attractions, including vibrant beaches like Calangute and Baga, historic churches like the Basilica of Bom Jesus, and thrilling water sports.  ",
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-tvV7MbpxGdfmHpkMSVySrtAFtAABmLv6vA&s",
+        Price : "29,658",  
+    },
+    {
+      id : 4,
+      name : "Varanasi",
+        info : "Varanasi, also known as Banaras or Kashi, is a major Indian pilgrimage city and a popular tourist destination, renowned for its spiritual significance and rich cultural heritage. It is one of the oldest continuously inhabited cities in the world, attracting millions of tourists annually.The ghats, steps leading down to the Ganges River, are central to Varanasi's identity, with Dashashwamedh Ghat and Manikarnika Ghat being particularly famous. ",
+        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Ahilya_Ghat_by_the_Ganges%2C_Varanasi.jpg/1200px-Ahilya_Ghat_by_the_Ganges%2C_Varanasi.jpg",
+        Price : "92,345",  
+    },
+    {
+      id : 5,
+      name : "Darjeeling",
+        info : "Darjeeling, known as the Queen of the Hills, is a popular tourist destination in West Bengal, India, famous for its tea plantations, scenic beauty, and the Darjeeling Himalayan Railway. It offers a blend of natural landscapes, cultural experiences, and adventure activities. The town offers breathtaking views of the Himalayas, including the majestic Kanchenjunga peak. Tiger Hill is a popular spot for sunrise views.  ",
+        image : "https://s7ap1.scene7.com/is/image/incredibleindia/2-summer-capital-of-India-darjeeling-west-bengal-city-ff?qlt=82&ts=1726643695016",
+        Price : "30,200",  
+    },
+    {
+      id : 6,
+      name : " Jaisalmer",
+        info : "Jaisalmer, is an ancient town at the western edge of Thar desert in Rajasthan has now become an attraction for desert adventure tourist. The town is known as 'Golden city' because it is build with yelow sand stone located around the renowned Jaisalmer fort, Havelis in old town and monsoon lake Gadisagar at the south-east corner.The Golden city of Jaisalmer was built by Bhati dynasty dating back from the mid 12th century. Jaisalmer is the huge center of Jain, Rajput and Brahmanical culture. ",
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxhEBTGfhxu0z9nz3NDRghYm11XpuLjy-msg&s",
+        Price : "44,450",  
+    },
+    {
+      id : 7,
+      name : "Uttarakhand",
+        info : "Uttarakhand, also known as Devbhumi or the Land of the Gods, is a popular tourist destination in India, renowned for its Himalayan beauty, spiritual significance, and diverse wildlife. The state attracts a large number of domestic tourists, particularly for its hill stations, pilgrimage sites, and national parks.Uttarakhand offers stunning Himalayan scenery, including snow-capped peaks, glaciers, and rivers like the Ganga. Popular activities include trekking, river rafting, skiing, and camping.  ",
+        image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJMrU3uD-w5Hfbh55BUIwJgb02HyOso_x95w&s",
+        Price : "56,900",  
+    }
+]
